07ca5f858b1f6eaef42a114a8db9f6aafa9d70af
unknown <you@example.com>
2025-02-26 22:32:50 -0300

    all microfrontends are working

--- app-about/src/app/app.component.ts
+++ app-about/src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  //standalone: true,
-  //imports: [RouterOutlet],
+  selector: 'app-about',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app-about';
--- app-about/src/main.single-spa.ts
+++ app-about/src/main.single-spa.ts
@@ -1,6 +1,4 @@
 import { enableProdMode, NgZone } from '@angular/core';
-
-//import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { Router, NavigationStart } from '@angular/router';
 
@@ -10,7 +8,6 @@ import { provideRouter } from '@angular/router';
 import { environment } from './environments/environment';
 import { singleSpaPropsSubject } from './single-spa/single-spa-props';
 import { APP_BASE_HREF } from '@angular/common';
-import { AppModule } from './app/app.module';
 import { AppComponent } from './app/app.component';
 import { EmptyRouteComponent } from './app/empty-route/empty-route.component';
 
@@ -30,53 +27,12 @@ const lifecycles = singleSpaAngular({
     return bootstrapApplication(AppComponent, { providers: extraProviders });
 
   },
-  template: '<app-root />',
+  template: '<app-about />',
   Router,
   NavigationStart,
   NgZone,
 });
 
-
-
 export const bootstrap = lifecycles.bootstrap;
 export const mount = lifecycles.mount;
 export const unmount = lifecycles.unmount;
-
-
-
-/*
-import { enableProdMode, NgZone } from '@angular/core';
-
-import { getSingleSpaExtraProviders, singleSpaAngular } from 'single-spa-angular';
-
-import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-import { singleSpaPropsSubject } from './single-spa/single-spa-props';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { APP_BASE_HREF } from '@angular/common';
-import { EmptyRouteComponent } from './app/empty-route/empty-route.component';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-const lifecycles = singleSpaAngular({
-  bootstrapFunction: singleSpaProps => {
-    singleSpaPropsSubject.next(singleSpaProps);
-    return bootstrapApplication(AppComponent, {
-      providers: [
-        getSingleSpaExtraProviders(),
-        provideRouter([{ path: '**', component: EmptyRouteComponent }]),
-        { provide: APP_BASE_HREF, useValue: '/' },
-      ],
-    });
-  },
-  template: '<app-root />',
-  NgZone,
-});
-
-export const bootstrap = lifecycles.bootstrap;
-export const mount = lifecycles.mount;
-export const unmount = lifecycles.unmount;
-*/
